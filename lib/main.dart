0872709b143a62cf18620d2c255688ba0d40b162
Lennon Henk <l_henk@hotmail.com>
2019-10-19 14:58:31 -0300

    App completo

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:buscador_gif/ui/HomePage.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(hintColor: Colors.white),
+
+  ));
+
+}
